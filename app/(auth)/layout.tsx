f6806043bf5b80561e818f0ae5b93eb8569c4676
mchena <m.chena@itecriocuarto.org.ar>
2025-11-26 21:37:15 -0300

    AuthLayout modified

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function AuthLayout({children}: {children: React.ReactNode}){
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className='grid lg:grid-cols-2 h-full items-center justify-center'>
-        <div className='flex items-center justify-center'>{children}</div>
-        <div className='hidden lg:flex lg:bg-slate-300 h-full justify-center items-center lg:flex-col'>
-          <Image src="/logo.svg" alt="Logo Cars" width="80" height="80"/>
-          <h1 className="text-xl font-bold">Cars</h1>
-        </div>
+    <div className="grid lg:grid-cols-2 h-full items-center justify-center">
+      <div className="flex items-center justify-center">{children}</div>
+      <div className="hidden lg:flex lg:bg-slate-300 h-full justify-center items-center lg:flex-col">
+        <Image src="/logo.svg" alt="Logo TarreCars" width="80" height="80" />
+        <h1 className="text-xl font-bold">Rent Cars</h1>
+      </div>
     </div>
-  )
+  );
 }
