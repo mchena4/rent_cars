301589827b306f8a5661072c01ffa3844187d370
mchena <m.chena@itecriocuarto.org.ar>
2025-11-26 21:39:37 -0300

    Dashboard page

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,10 @@
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">List of cars</h2>
+      </div>
+      <p>List</p>
+    </div>
   )
 }
