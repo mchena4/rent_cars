ae7ec9f145b36fde6c1033517f51b292f6292a3d
mchena <m.chena@itecriocuarto.org.ar>
2025-11-26 21:41:03 -0300

    Problem with Clerk provider fixed

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Admin Cars",
+  description: "Cars eccomerce",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <NextTopLoader color="#000"/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
