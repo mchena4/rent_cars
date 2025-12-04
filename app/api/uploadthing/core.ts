32d18a290b8bde3fcc61c1d7c01778ba0cd44ba4
mchena <m.chena@itecriocuarto.org.ar>
2025-12-04 01:08:05 -0300

    auth() error fixed

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,16 +1,22 @@
-import { useAuth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from "@clerk/nextjs/server";
+
 const f = createUploadthing();
 
-const HandleAuth = ()=> {
-    const { userId } = useAuth();
-    if (!userId) throw new Error("Unauthorized");
-    return { userId };
-}
+const handleAuth = () => {
+  const userId  = auth();
+  
+  if (!userId) throw new Error("Unauthorized");
+  return { userId };
+};
 
 export const ourFileRouter = {
-    photo: f({image: { maxFileSize: "4MB", maxFileCount: 1 }})
-    .middleware(() => HandleAuth())
-    .onUploadComplete(() => {}),
+  photo: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(async () => handleAuth())
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("user:", metadata.userId);
+      console.log("URL:", file.ufsUrl);
+    }),
 } satisfies FileRouter;
+
 export type OurFileRouter = typeof ourFileRouter;
