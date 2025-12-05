6554783a27d2828af14e9446d792ff675f31a0f5
mchena <m.chena@itecriocuarto.org.ar>
2025-12-05 19:57:50 -0300

    Validations of form updated

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/FormAddCar/FormAddCar.form.ts
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/FormAddCar/FormAddCar.form.ts
@@ -2,12 +2,12 @@ import { z } from "zod"
 
 export const formSchema = z.object({
     name: z.string().min(2).max(50),
-    cv: z.string().min(2).max(50),
+    cv: z.string().min(1).max(50),
     transmission: z.string().min(2).max(50),
-    people: z.string().min(2).max(50),
-    photo: z.string().min(2).max(50),
+    people: z.string().min(1).max(50),
+    photo: z.string().min(2),
     engine: z.string().min(2).max(50),
     type: z.string().min(2).max(50),
-    priceDay: z.string().min(2).max(50),
+    priceDay: z.string().min(1).max(50),
     isPublish: z.boolean(),
 })
