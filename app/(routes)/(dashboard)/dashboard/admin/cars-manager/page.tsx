a6c716a5f0947115a3614e016dd689aa8146925b
mchena <m.chena@itecriocuarto.org.ar>
2025-12-09 22:28:29 -0300

    List of cars component added

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
@@ -1,12 +1,34 @@
+import { auth } from "@clerk/nextjs/server";
 import { ButtonAddCar } from "./components/ButtonAddCar/ButtonAddCar";
+import { ListCars } from "./components/ListCars/ListCars";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
 
-export default function CarsManagerPage() {
+export default async function CarsManagerPage() {
+
+    const {userId} = await auth()
+
+    if (!userId) {
+        return redirect('/')
+    }
+
+    const car = await prisma.car.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    });
+
+
     return(
         <div>
             <div className="flex justify-between">
-                <h2 className="text-2xl font-bold">Mange your cars</h2>
+                <h2 className="text-2xl font-bold">Manage your cars</h2>
                 <ButtonAddCar />
             </div>
+            <ListCars cars={car}></ListCars>
         </div>
     )
 }
