c4d7ca016883acdc58c8bd3f2f7a22779dbdf8f5
mchena <m.chena@itecriocuarto.org.ar>
2025-12-17 20:09:44 -0300

    API for edit car

---
+++ app/api/car/[carId]/form/route.ts
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { carId: string } }
+    )  {
+        try {
+            const { userId } = await auth();
+            const { carId } = params 
+            const values = await req.json();
+
+            if (!userId) {
+                return new NextResponse("Unauthorized", { status: 401 });
+            }
+
+            if (!carId) {
+                return new NextResponse("Car ID is required", { status: 400 });
+            }
+
+            const car = await prisma.car.update({
+                where: {
+                    id: carId,
+                    userId: userId,
+                },
+                data: {
+                    ...values
+                }
+            });
+
+            return NextResponse.json(car)
+            
+        } catch (error) {
+            console.log("CAR ID", error)
+            return new NextResponse("Internal Server Error", { status: 500 });
+        }
+}
