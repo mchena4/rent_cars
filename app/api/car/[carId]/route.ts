817660cfa1d8e93dc5ed987889ffe7f36522aa89
mchena <m.chena@itecriocuarto.org.ar>
2025-12-19 19:51:00 -0300

    request PATCH for carId

--- app/api/car/[carId]/route.ts
+++ app/api/car/[carId]/route.ts
@@ -2,6 +2,40 @@ import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+export async function PATCH(
+    req: Request,
+    { params }: {params: Promise<{ carId: string}>}  
+    ) {
+        try {
+            const { userId } = await auth()
+            const { carId } =  await params
+            const { isPublished } = await req.json()
+            
+            if (!userId){
+                return new NextResponse("Unauthorized", { status: 401})
+            }
+            
+            if (!carId){
+                return new NextResponse("Car ID is required", { status: 400}) 
+            }
+
+            const car = await prisma.car.update({
+                where: {
+                    id: carId,
+                    userId
+                },
+                data: {
+                    isPublish: isPublished
+                }
+            })
+            return NextResponse.json(car)
+        } catch (error) {
+            console.log(error)
+            return new NextResponse("Internal error", { status: 500 })
+        }
+    }
+
+
 export async function DELETE(
     req: Request,
     { params }: { params: Promise<{ carId: string }> }
