8b03a6f6345e7fb7abce92ba6e83cc72f6c45ee2
mchena <m.chena@itecriocuarto.org.ar>
2025-12-21 20:25:25 -0300

    Component ListCars added

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,33 @@
-export default function DashboardPage() {
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import prisma from "@/lib/prisma"
+
+import { ListCars } from "./components/ListCars"
+
+export default async function DashboardPage() {
+  
+  const { userId } = await auth()
+
+  if (!userId){
+    redirect('/')
+  }
+
+  const cars = await prisma.car.findMany({
+    where: {
+      isPublish: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  });
+  
   return (
     <div>
       <div className="flex justify-between">
-        <h2 className="text-2xl font-bold">List of cars</h2>
+        <h2 className="text-2xl font-bold">
+          <ListCars cars={cars} />
+        </h2>
       </div>
-      <p>List</p>
     </div>
   )
 }
